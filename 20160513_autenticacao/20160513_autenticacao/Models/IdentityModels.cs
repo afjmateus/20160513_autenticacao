32ec01d88f535d847f6a2a40d5e7b3a7eb83a4be
A. Mateus <a_mateus@msn.com>
2016-05-13 20:07:27 +0100

    adição de atributos especificos ao USER (Nome Próprio, Apelido, NIF)

--- 20160513_autenticacao/20160513_autenticacao/Models/IdentityModels.cs
+++ 20160513_autenticacao/20160513_autenticacao/Models/IdentityModels.cs
@@ -3,14 +3,23 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
-namespace _20160513_autenticacao.Models
-{
+namespace _20160513_autenticacao.Models {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
+    public class ApplicationUser : IdentityUser {
+
+        // adicionar atributos ao USER
+        [Display(Name = "Nome próprio")]
+        [Required]
+        public string NomeProprio { get; set; }
+
+        [Required]
+        public string Apelido { get; set; }
+
+        public string NIF { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
@@ -18,15 +27,12 @@ namespace _20160513_autenticacao.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
+            : base("DefaultConnection", throwIfV1Schema: false) {
         }
 
-        public static ApplicationDbContext Create()
-        {
+        public static ApplicationDbContext Create() {
             return new ApplicationDbContext();
         }
     }

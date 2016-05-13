89125518dea81c3528aa293b689e5004ed8fcea9
A. Mateus <a_mateus@msn.com>
2016-05-13 17:54:41 +0100

    1st commit - new project

---
+++ 20160513_autenticacao/20160513_autenticacao/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _20160513_autenticacao {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
